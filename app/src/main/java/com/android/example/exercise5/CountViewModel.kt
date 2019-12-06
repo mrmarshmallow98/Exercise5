55f97172076dc0510b649367b2ae2540aa9a2370
mrmarshmallow98 <ahjie981127@gmail.com>
2019-12-06 09:59:56 +0800

    Initial commit

---
+++ app/src/main/java/com/android/example/exercise5/CountViewModel.kt
@@ -0,0 +1,10 @@
+package com.android.example.exercise5
+
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel() {
+
+    var countLike: Int = 0
+    var countDislike: Int =0
+
+}
